241ff4556cee6b8c9eaf44ff0c20991a027f7183
jake-is-ESD-protected <jts4681@students.fhv.at>
2022-09-09 16:49:54 +0200

    share with karla; BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,9 @@
 #include <Arduino.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include "timer.h"
-
-ISR(RTC_PIT_vect) {
-    // routine goes here
-    led.toggle();
-    RTC.PITINTFLAGS = 1; // reset interrupt
-}
-
 
 void setup() {
-  Serial.begin(115200);
-  tim.init(1000);
+  // put your setup code here, to run once:
 }
 
-void loop()
-{
-
+void loop() {
+  // put your main code here, to run repeatedly:
 }
