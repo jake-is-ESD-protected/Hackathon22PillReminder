2d5e7732a49138ee442e35575865b82870bdc6a7
jake-is-ESD-protected <jts4681@students.fhv.at>
2022-09-09 17:46:46 +0200

    last share

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "timer.h"
+
+ISR(RTC_PIT_vect) {
+    // routine goes here
+    led.toggle();
+    RTC.PITINTFLAGS = 1; // reset interrupt
+}
+
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  tim.init(1000);
 }
 
 void loop() {
