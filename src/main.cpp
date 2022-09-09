7e8452e6da300764c89caeee5223f65bf8109423
jake-is-ESD-protected <jts4681@students.fhv.at>
2022-09-09 14:38:44 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include "avr8-stub.h"
+#include "app_api.h"
 
 void setup() {
+  debug_init();
+  pinMode(LED_BUILTIN, OUTPUT);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
